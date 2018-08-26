94b8c31ee6ef7f12556df79cb4451493dd8bd6f9
Rohit Kumar <rohit9075@gmail.com>
2018-08-26 23:10:00 +0530

    Initial commit

---
+++ app/src/main/java/com/rohit/lpregister/database/Constants.java
@@ -0,0 +1,19 @@
+package com.rohit.lpregister.database;
+
+class Constants {
+    
+    
+    static final int DATABASE_VERSION = 1;
+    static final String DATABASE_NAME = "candidate.db";
+    
+    
+    static final String TABLE_CANDIDATE = "candidate";
+    static final String COLUMN_CANDIDATE_FIRST_NAME = "candidate_firstname";
+    static final String COLUMN_CANDIDATE_LAST_NAME = "candidate_lastname";
+    static final String COLUMN_CANDIDATE_DATE_OF_BIRTH = "candidate_dateofbirth";
+    static final String COLUMN_CANDIDATE_EMAIL = "candidate_email";
+    static final String COLUMN_CANDIDATE_PHONE="candidate_phone";
+    static final String COLUMN_CANDIDATE_PASSWORD = "candidate_password";
+    static final String COLUMN_CANDIDATE_GENDER= "candidate_gender";
+    static final String COLUMN_CANDIDATE_IMAGE= "candidate_image";
+}
